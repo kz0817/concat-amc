e87bd9123ac9f7d1361723048a9c535a547d6f3d
Kazuhiro Yamato <kz0817@gmail.com>
2016-01-03 21:31:56 +0900

    Temp save.

--- calc-gaps/CalcGaps.scala
+++ calc-gaps/CalcGaps.scala
@@ -1,6 +1,6 @@
 object ClacGaps {
 
-  val src = Array(
+  val src = List(
     Map("title" -> "disc1.1",
         "video" -> "01:45:15.309", "audio" -> "01:45:14.705"),
     Map("title" -> "disc1.2",
@@ -15,7 +15,38 @@ object ClacGaps {
         "video" -> "00:15:34.934", "audio" -> "00:15:34.930")
   );
 
+  val SamplingRate = 48 * 1000.0;
+
+
+  def sec(time: String): Double = {
+    time split(":") zip(List(3600, 60, 1)) map {
+      t => t._1.toDouble * t._2 } sum
+  }
+
+  def showTotalTime() {
+    val videoLen = (src map { _("video") } map sec sum)
+    val audioLen = (src map { _("audio") } map sec sum)
+    printf("Total: video: %.6f, audio: %.6f, diff: %.6f (sec)%n",
+           videoLen, audioLen, (videoLen - audioLen))
+  }
+
+  def numSamples(time: Double): Double = {
+    SamplingRate * time
+  }
+
+  def showDiffs() {
+    val videoLenList = src map { _("video") } map sec
+    val audioLenList = src map { _("audio") } map sec
+    val diffList = videoLenList zip(audioLenList) map { t => t._1 - t._2 }
+    val samplesList = diffList map numSamples
+    src map { _("title") } zip diffList zip samplesList foreach { x =>
+      printf("title: %s, diff: %.6f, count: %.1f%n",
+             x._1._1, x._1._2, x._2)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    src foreach println
+    showTotalTime()
+    showDiffs()
   }
 }
